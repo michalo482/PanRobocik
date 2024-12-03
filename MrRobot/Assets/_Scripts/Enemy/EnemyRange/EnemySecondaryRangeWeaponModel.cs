f237bf14c27fe48a139fe4a56d65c4d32e98086b
michalo482 <dakkon@o2.pl>
2024-12-03 10:14:12 +0100

    test test test

--- MrRobot/Assets/_Scripts/Enemy/EnemyRange/EnemySecondaryRangeWeaponModel.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemySecondaryRangeWeaponModel : MonoBehaviour
-{
-   public EnemyRangeWeaponType weaponType;
-}
